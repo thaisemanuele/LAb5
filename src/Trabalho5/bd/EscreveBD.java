c01da629c56e5181919483398c13ef0d1076cb1c
thatitis <thatitis@gmail.com>
2014-11-21 17:51:33 -0200

    Escreve -> Insere, Busca

--- src/Trabalho5/bd/EscreveBD.java
+++ src/Trabalho5/bd/EscreveBD.java
@@ -5,9 +5,11 @@
  */
 package Trabalho5.bd;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.ParseException;
+import javax.swing.DefaultComboBoxModel;
 
 /**
  *
@@ -20,6 +22,32 @@ public class EscreveBD {
         System.out.println("insert statement " + insert);
         Statement statement = dbConnection.createStatement();
         statement.executeUpdate(insert);
+    }
+    
+    public static DefaultComboBoxModel getArtigos(Integer idAut) throws SQLException, ParseException{
+        DefaultComboBoxModel model = new DefaultComboBoxModel();
+        String select = "SELECT idArt FROM Escreve WHERE idAut = " +idAut;
+        System.out.println("select statement " + select);
+        Statement statement = dbConnection.createStatement();
+        ResultSet rs = statement.executeQuery(select);
+        model.addElement(" --- ");
+        while(rs.next()){
+            model.addElement(rs.getString("idArt"));
+        }
+        return model;
+    }
+    
+    public static DefaultComboBoxModel getAuthors(Integer idArt) throws SQLException, ParseException{
+        DefaultComboBoxModel model = new DefaultComboBoxModel();
+        String select = "SELECT idAut FROM Escreve WHERE idArt = " +idArt;
+        System.out.println("select statement " + select);
+        Statement statement = dbConnection.createStatement();
+        ResultSet rs = statement.executeQuery(select);
+        model.addElement(" --- ");
+        while(rs.next()){
+            model.addElement(PessoaBD.getEmailById(Integer.parseInt(rs.getString("idAut"))));
+        }
+        return model;
     }
     
 }
