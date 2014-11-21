9ae606f65376979166f93fd91f61ac6f6ee5ffe8
thatitis <thatitis@gmail.com>
2014-11-21 14:14:38 -0200

    Escreve -> Insere

--- src/Trabalho5/bd/EscreveBD.java
+++ src/Trabalho5/bd/EscreveBD.java
@@ -5,10 +5,21 @@
  */
 package Trabalho5.bd;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+
 /**
  *
  * @author Thais
  */
 public class EscreveBD {
+    
+    public static void inserir(Integer idAut, Integer idArt) throws SQLException, ParseException{
+        String insert = "INSERT INTO Escreve VALUES( " +idAut+ ", "+idArt+")";
+        System.out.println("insert statement " + insert);
+        Statement statement = dbConnection.createStatement();
+        statement.executeUpdate(insert);
+    }
     
 }
--- src/Trabalho5/telas/TelaEscreve.java
+++ src/Trabalho5/telas/TelaEscreve.java
@@ -7,6 +7,7 @@
 package Trabalho5.telas;
 
 import Trabalho5.bd.ArtigoBD;
+import Trabalho5.bd.EscreveBD;
 import Trabalho5.bd.EventoBD;
 import Trabalho5.bd.PessoaBD;
 import java.sql.SQLException;
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -45,7 +47,7 @@ public class TelaEscreve extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        insertEscreveButton = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         selectEscreveAutor = new javax.swing.JComboBox();
         selectEscreveArtigo = new javax.swing.JComboBox();
@@ -91,10 +93,10 @@ public class TelaEscreve extends javax.swing.JFrame {
 
         jLabel3.setText("Artigo:");
 
-        jButton1.setText("Inserir");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        insertEscreveButton.setText("Inserir");
+        insertEscreveButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                insertEscreveButtonActionPerformed(evt);
             }
         });
 
@@ -142,7 +144,7 @@ public class TelaEscreve extends javax.swing.JFrame {
                         .addGroup(escreveInserirLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(escreveInserirLayout.createSequentialGroup()
                                 .addGap(174, 174, 174)
-                                .addComponent(jButton1)
+                                .addComponent(insertEscreveButton)
                                 .addGap(18, 18, 18)
                                 .addComponent(jButton2))
                             .addGroup(escreveInserirLayout.createSequentialGroup()
@@ -189,7 +191,7 @@ public class TelaEscreve extends javax.swing.JFrame {
                 .addComponent(insertArtTitle, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 113, Short.MAX_VALUE)
                 .addGroup(escreveInserirLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
+                    .addComponent(insertEscreveButton)
                     .addComponent(jButton2))
                 .addGap(48, 48, 48))
         );
@@ -228,11 +230,10 @@ public class TelaEscreve extends javax.swing.JFrame {
         escreveBuscarLayout.setHorizontalGroup(
             escreveBuscarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(escreveBuscarLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel5)
-                .addGap(0, 360, Short.MAX_VALUE))
-            .addGroup(escreveBuscarLayout.createSequentialGroup()
                 .addGroup(escreveBuscarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(escreveBuscarLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel5))
                     .addGroup(escreveBuscarLayout.createSequentialGroup()
                         .addContainerGap()
                         .addGroup(escreveBuscarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -253,7 +254,7 @@ public class TelaEscreve extends javax.swing.JFrame {
                     .addGroup(escreveBuscarLayout.createSequentialGroup()
                         .addGap(201, 201, 201)
                         .addComponent(jButton4)))
-                .addGap(0, 0, 0))
+                .addGap(0, 21, Short.MAX_VALUE))
         );
         escreveBuscarLayout.setVerticalGroup(
             escreveBuscarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -457,9 +458,20 @@ public class TelaEscreve extends javax.swing.JFrame {
         dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void insertEscreveButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_insertEscreveButtonActionPerformed
+        if(selectEscreveAutor.getSelectedIndex()==0||selectEscreveArtigo.getSelectedIndex()==0) return;
+        try {
+            Integer idAut = PessoaBD.getIdByEmail(selectEscreveAutor.getSelectedItem().toString());
+            Integer idArt = Integer.parseInt(selectEscreveArtigo.getSelectedItem().toString());
+            EscreveBD.inserir(idAut, idArt);
+            JOptionPane.showMessageDialog(null, "Escritor cadastrado " , "Cadastro", JOptionPane.INFORMATION_MESSAGE);
+            clearFields();
+        } catch (SQLException ex) {
+            Logger.getLogger(TelaEscreve.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(TelaEscreve.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_insertEscreveButtonActionPerformed
 
     private void selectEscreveAutorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_selectEscreveAutorActionPerformed
         if(selectEscreveAutor.getSelectedIndex()==0) return;
@@ -484,6 +496,12 @@ public class TelaEscreve extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_selectEscreveArtigoActionPerformed
 
+    private void clearFields(){
+        selectEscreveAutor.setSelectedIndex(0);
+        selectEscreveArtigo.setSelectedIndex(0);
+        insertAuthorName.setText("");
+        insertArtTitle.setText("");
+    }
     private void loadComboBox(){
         DefaultComboBoxModel autModel = new DefaultComboBoxModel();
         DefaultComboBoxModel artModel = new DefaultComboBoxModel();
@@ -541,7 +559,7 @@ public class TelaEscreve extends javax.swing.JFrame {
     protected javax.swing.JTabbedPane escreveTPane;
     private javax.swing.JTextField insertArtTitle;
     private javax.swing.JTextField insertAuthorName;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton insertEscreveButton;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
