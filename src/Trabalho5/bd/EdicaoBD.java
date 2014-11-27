3c89b6f7da7dbc32c5dfda8fba3c6aaee7530f19
Clayton-Oliveira <claylionhearth@gmail.com>
2014-11-27 03:06:52 -0200

    artigoBD, auxilioBD, despesaBD,EdicaoBD e EscreveBD documentados

--- src/Trabalho5/bd/EdicaoBD.java
+++ src/Trabalho5/bd/EdicaoBD.java
@@ -39,6 +39,18 @@ public class EdicaoBD {
         this.taxaEd = taxaEd;
     }
   
+    /**
+     * Cria e executa um insert statement
+     * @param codEv -codigo do evento
+     * @param numEd - numero da edicao
+     * @param descricaoEd - descrição do evento
+     * @param dataInicioEd - data de inicio da edição do evento
+     * @param dataFimEd -  data de fim da edição do evento
+     * @param localEd -  local da edição
+     * @param taxaEd - taxa da edição
+     * @throws SQLException - se houver algum erro com o sql statemente
+     * @throws ParseException - se a data estiver no formato incorreto
+     */
     public static void inserir(Integer codEv, Integer numEd, String descricaoEd, 
             String dataInicioEd, String dataFimEd, String localEd, String taxaEd) throws SQLException, ParseException{
         String insert = "INSERT INTO Edicao VALUES( " 
@@ -49,6 +61,17 @@ public class EdicaoBD {
 
     }
     
+    /**
+     * Cria e executa um update statement
+     * @param codEv
+     * @param numEd
+     * @param descricaoEd
+     * @param dataInicioEd
+     * @param dataFimEd
+     * @param localEd
+     * @param taxaEd
+     * @throws SQLException
+     */
     public static void atualizar(Integer codEv, Integer numEd,String descricaoEd, 
             String dataInicioEd, String dataFimEd, String localEd, String taxaEd) throws SQLException{
         String update = "UPDATE Edicao SET descricaoEd = '" 
@@ -60,12 +83,26 @@ public class EdicaoBD {
         statement.executeUpdate(update);
     }
     
+    /**
+     * Cria e executa um delete stqtemente de acordo com a entrada
+     * @param codEv
+     * @param numEd
+     * @throws SQLException
+     */
     public static void excluir(Integer codEv, Integer numEd) throws SQLException{
         String delete = "DELETE FROM Edicao WHERE numEd = '" + numEd + "' and codEv = '" + codEv + "'";
         Statement statement = dbConnection.createStatement();
         statement.executeUpdate(delete);
     }
     
+    /**
+     * Cria e retorna um arraylist com os dados de um evento de acordo com os parametros de entrada
+     * @param codEv
+     * @param numEd
+     * @return details - arraylist com os dados de um evento
+     * @throws SQLException
+     * @throws ParseException
+     */
     public static ArrayList<String> buscar(Integer codEv, Integer numEd) throws SQLException, ParseException{
         String selectTableSQL = "SELECT codEv, numEd, descricaoEd, TO_CHAR(dataInicioEd, 'DD-MM-YYYY'), "
                 + "TO_CHAR(dataFimEd, 'DD-MM-YYYY'), localEd, TO_CHAR ( taxaEd, 'FML999G999D99'), saldoFinanceiroEd, qtdArtigosApresentadosEd"
@@ -81,7 +118,12 @@ public class EdicaoBD {
         return details;
     }
     
-    
+    /**
+     * Cria e retorna uma combobox com os numeros de edicao de acordo com o parametro de entrada
+     * @param codEv - codigo do evento
+     * @return model - combobox com os numeros de edicao
+     * @throws SQLException
+     */
     public static DefaultComboBoxModel getEditions(Integer codEv) throws SQLException{
          int i=1;
         DefaultComboBoxModel model = new DefaultComboBoxModel();
